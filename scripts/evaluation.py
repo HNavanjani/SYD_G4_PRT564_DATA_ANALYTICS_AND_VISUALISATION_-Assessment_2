2d63abb0c180e1b4602e14688478a8aa4a3d02c8
ZEESHAN-KHALID\bisma <bismahfaez@gmail.com>
2025-04-23 01:00:45 +1000

    Add initial model evaluation script

---
+++ scripts/evaluation.py
@@ -0,0 +1,21 @@
+import joblib
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
+
+X_test = joblib.load("outputs/results/X_test.pkl")
+y_test = joblib.load("outputs/results/y_test.pkl")
+
+lr = joblib.load("outputs/results/linear_model.pkl")
+rf = joblib.load("outputs/results/rf_model.pkl")
+
+lr_pred = lr.predict(X_test)
+rf_pred = rf.predict(X_test)
+
+def evaluate(name, y_true, y_pred):
+    print(f"\n{name} Performance:")
+    print("R² Score: ", r2_score(y_true, y_pred))
+    print("RMSE:     ", np.sqrt(mean_squared_error(y_true, y_pred)))
+    print("MAE:      ", mean_absolute_error(y_true, y_pred))
+
+evaluate("Linear Regression", y_test, lr_pred)
+evaluate("Random Forest", y_test, rf_pred)
